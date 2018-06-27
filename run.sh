56bb917ea22167c47dbd68858d39019b2360e835
Himchan Park <chan150@dgist.ac.kr>
2018-06-27 21:40:31 +0900

    fix configurations

--- run.sh
+++ run.sh
@@ -17,6 +17,9 @@ HDFS_HOME=`hadoop fs -df | grep hdfs | awk '{print $1}'`/user/$USER/
 
 spark-submit --master spark://$MASTER:$PORT --class kr.acon.EvoGraph \
  --conf spark.network.timeout=20000000ms \
+ --conf spark.hadoop.dfs.replication=1 \
+ --conf spark.driver.userClassPathFirst=true \
+ --conf spark.executor.userClassPathFirst=true \
  --conf spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version=2 \
  --jars lib/fastutil-8.1.1.jar,lib/dsiutils-2.4.2.jar EvoGraph.jar \
  -gs.input toy -format tsv -hdfs $HDFS_HOME $@
